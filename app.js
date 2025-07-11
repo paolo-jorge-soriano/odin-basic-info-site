77e76245354b5cc334fd5f864b09d53a638d571a
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-07-11 17:32:50 +0800

    add app.js, which uses Express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
